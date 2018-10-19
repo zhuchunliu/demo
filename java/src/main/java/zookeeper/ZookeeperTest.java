53aa9e946db9a5543befc2725cea2e2028de7b71
zhuchunliu <296898883@qq.com>
2018-10-19 15:32:00 +0800

    zk test

--- java/src/main/java/zookeeper/ZookeeperTest.java
+++ java/src/main/java/zookeeper/ZookeeperTest.java
@@ -8,17 +8,31 @@ import org.apache.zookeeper.data.Stat;
  */
 public class ZookeeperTest {
 
+    private void testChildren() throws Exception{
+        ZooKeeper zk = new ZooKeeper("localhost:4180", 50000, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                System.err.println("节点："+watchedEvent.getPath()+", "+watchedEvent.getType()+" 事件触发了");
+            }
+        });
 
-    /*
-        Watcher & Version
-        watcher分为两大类：data watches和child watches。getData()和exists()上可以设置data watches，getChildren()上可以设置child watches。
-        setData()会触发data watches;
-        create()会触发data watches和child watches;
-        delete()会触发data watches和child watches.
-        如果对一个不存在的节点调用了exists()，并设置了watcher，而在连接断开的情况下create/delete了该znode，则watcher会丢失。
-        在server端用一个map来存放watcher，所以相同的watcher在map中只会出现一次，只要watcher被回调一次，它就会被删除----map解释了watcher的一次性。
-            比如如果在getData()和exists()上设置的是同一个data watcher，调用setData()会触发data watcher，但是getData()和exists()只有一个会收到通知。
-     */
+        zk.create("/root","root".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.getChildren("/root",true);//监控/root子节点的创建、删除行为
+        zk.exists("/root/child",true);//监控/root/child创建行为
+        zk.create("/root/child","child".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //【节点：/root/child, NodeCreated 事件触发了】【节点：/root, NodeChildrenChanged 事件触发了】
+
+        zk.exists("/root",true);//设置/root的data行为
+        zk.exists("/root/child",true);//需要重新绑定事件，create已经把事件消费掉了
+        zk.getChildren("/root",true);//监控/root子节点行为，之前create已经消费掉了
+        zk.delete("/root/child", -1);//【节点：/root/child, NodeDeleted 事件触发了】【节点：/root, NodeChildrenChanged 事件触发了】,
+        // 不会触发/root数据watcher
+
+        zk.delete("/root", -1);//【节点：/root/child, NodeDeleted 事件触发了】
+        zk.close();// 【节点：null, None 事件触发了】
+
+    }
+
     private void testZoo() throws Exception{
         ZooKeeper zk = new ZooKeeper("localhost:4180", 50000, new Watcher() {
             @Override
@@ -27,22 +41,17 @@ public class ZookeeperTest {
             }
         });
 
-        zk.create("/testRootPath","testRootData".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        Stat stat1 = zk.exists("/testRootPath",true);// 此处不用设置watcher，之前已经设置过了
-//        zk.getChildren("/testRootPath",true);
-//        zk.getData("/testRootPath",true,stat1);
-//        Stat stat = zk.exists("/testRootPath",true);
-//        Stat stat1 = zk.exists("/testRootPath",false);// 此处不用设置watcher，之前已经设置过了
-        zk.setData("/testRootPath","abc".getBytes(),stat1.getVersion());
-        zk.exists("/testRootPath",true);
-        zk.delete("/testRootPath", -1);
-
-
-        zk.close();
-
+        zk.create("/root","root".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        Stat stat = zk.exists("/root",true);//设置true，使用构造函数设置的watcher
+        stat = zk.setData("/root","abc1".getBytes(),stat.getVersion());//触发NodeDataChanged事件，后面setData值没有watcher事件，因为事件已经被触发了
+        stat = zk.setData("/root","abc2".getBytes(),stat.getVersion());//收不到触发事件
+        zk.getData("/root",true,stat);//如果不重新绑定watcher事件，则下一步delete也不会触发事件
+        zk.delete("/root", -1);//触发触发NodeDataChanged事件事件，-1表示删除任何版本的/root
+        zk.close();//  在客户端与服务器断开连接的时候，触发None事件
 
     }
     public static void main(String[] args) throws Exception {
-        new ZookeeperTest().testZoo();
+//        new ZookeeperTest().testZoo();
+        new ZookeeperTest().testChildren();
     }
 }
